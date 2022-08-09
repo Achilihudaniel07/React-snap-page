90ec2fcb28a81d6d9ce1fd951425eb494ce02d6d
unknown <achilihudaniel53@gmail.com>
2022-08-09 10:18:35 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Practice from './Practice';
 import Navbar from './Components/Navbar/Navbar';
 import Body from './Components/Body/Body';
 
@@ -7,7 +6,6 @@ import Body from './Components/Body/Body';
 function App() {
   return (
     <div>
-     {/* <Practice/> */}
       <Navbar/>
       <Body/>
     </div>
