5172759c65ab793d6da42386ca86c6643fcc1d7d
AchilihuDaniel07 <achilihudaniel53@gmail.com>
2022-08-18 10:39:21 +0100

    third commit

---
+++ src/pages/App.js
@@ -0,0 +1,17 @@
+import '../pages/App.css';
+import Navbar from '../Components/Navbar/Navbar';
+import Body from '../Components/Body/Body';
+import Footer from '../Components/Footer/Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
